189c9981a2b3f775f0cd5bd777654e293c170275
Camila Fonseca <camilafonseca.ang7@gmail.com>
2022-02-09 12:53:00 -0300

    feat(pages): updated _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
 
 import { ThemeProvider } from '@xstyled/styled-components'
 
 import { GlobalStyle } from 'theme/globalStyle'
-import 'theme/preflight.css'
 import { theme } from 'theme/globalTheme'
+
+import 'react-toastify/dist/ReactToastify.css'
+import 'theme/preflight.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
+    <ToastContainer />
     <Component {...pageProps} />
   </ThemeProvider>
 )
